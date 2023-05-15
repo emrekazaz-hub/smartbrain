05aacf01af0dcbfe360f3575183319ce5e7a124f
emre <emre_kazaz_fb@hotmail.com>
2023-05-15 22:37:11 +0200

    Updates

---
+++ precache-manifest.eda98393a15574bb0b9c0342c0305dbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec6863e757bf1dfbb6e4",
+    "url": "/smartbrain-face-detection-full-stack/static/css/main.dba1117b.chunk.css"
+  },
+  {
+    "revision": "ec6863e757bf1dfbb6e4",
+    "url": "/smartbrain-face-detection-full-stack/static/js/main.ec6863e7.chunk.js"
+  },
+  {
+    "revision": "9d4f6bd9cc9576da336b",
+    "url": "/smartbrain-face-detection-full-stack/static/css/1.07ff0f50.chunk.css"
+  },
+  {
+    "revision": "9d4f6bd9cc9576da336b",
+    "url": "/smartbrain-face-detection-full-stack/static/js/1.9d4f6bd9.chunk.js"
+  },
+  {
+    "revision": "36abc933a37d26d7d06f",
+    "url": "/smartbrain-face-detection-full-stack/static/js/runtime~main.36abc933.js"
+  },
+  {
+    "revision": "2aa16ef917dd4218ef51fbcb37007797",
+    "url": "/smartbrain-face-detection-full-stack/static/media/WooX-2.2aa16ef9.png"
+  },
+  {
+    "revision": "59d7e774fafdda01b5d66dcc17653881",
+    "url": "/smartbrain-face-detection-full-stack/static/media/2644830.59d7e774.jpg"
+  },
+  {
+    "revision": "f74a30345cb8800bfe85634781809c0d",
+    "url": "/smartbrain-face-detection-full-stack/index.html"
+  }
+];
